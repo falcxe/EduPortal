06ecde53d54329f3283ae1b4fd58ac830b7c5b72
Falcxe <Sasiskakiller226@gmail.com>
2025-01-29 21:24:22 +0300

    обновление главной страницы

--- portal/main/static/main/js/create_course.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const testCheckbox = document.getElementById('has_test');
-    const testForm = document.querySelector('.test-form');
-
-    testCheckbox.addEventListener('change', function() {
-        testForm.style.display = this.checked ? 'block' : 'none';
-    });
-});
