da9f357f980c8b22945be5e75d803baec11975a6
Falcxe <Sasiskakiller226@gmail.com>
2025-01-29 21:11:15 +0300

    крутая главная страница

--- portal/main/views.py
+++ portal/main/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from courses.models import Course
 
 def index(request):
+    new_courses = Course.objects.order_by('-created_at')[:4]
+    recommended_courses = Course.objects.filter(author__username='falcxe')[:4]
 
-    return render(request, 'main/index.html')
+    context = {
+        'new_courses': new_courses,
+        'recommended_courses': recommended_courses
+    }
+    return render(request, 'main/index.html', context)
