2c3681983a258c90ee296f1e4bf483cb2c907be8
Falcxe <Sasiskakiller226@gmail.com>
2025-02-12 17:43:47 +0300

    bugfix wsgi.py

--- portal/portal/wsgi.py
+++ portal/portal/wsgi.py
@@ -12,11 +12,11 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 from pathlib import Path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal.portal.settings')
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 application = get_wsgi_application()
 
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
-
 application.add_files(os.path.join(BASE_DIR, 'media'), prefix='media/')
