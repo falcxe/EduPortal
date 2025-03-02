3a0af81bc213376c64d6278083a220c34d5b991c
Falcxe <Sasiskakiller226@gmail.com>
2025-03-02 19:34:53 +0300

    Update ping workflow schedule to run every minute

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 LOGIN_URL = 'login'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['eduportal-d2gf.onrender.com', '127.0.0.1']
 
